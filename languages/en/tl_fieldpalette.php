b5f3d5bbae99286c85bf1435efae56d578857512
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-08-12 16:15:22 +0200

    added english locales

---
+++ languages/en/tl_fieldpalette.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2016 Heimrich & Hannot GmbH
+ *
+ * @package fieldpalette
+ * @author  Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_fieldpalette']['published'] = array('Publish element', 'Show the element on the website.');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['start'] = array('Show from', 'Do not show the element on the website before this day.');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['stop'] = array('Show until', 'Do not show the element on the website on and after this day.');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_fieldpalette']['published_legend'] = 'Publish';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_fieldpalette']['new'] = array('New element','Create new element');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['edit']	= array('Edit element','Edit element ID %s');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['copy'] = array('Copy element','Copy element ID %s');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['delete']= array('Delete element löschen','Delete element ID %s');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['toggle'] = array('Publish/unpublish element','Publish/unpublish element ID %s');
+$GLOBALS['TL_LANG']['tl_fieldpalette']['show'] = array('Show element','Show details of element ID %s');
+
+/**
+ * Misc
+ */
+$GLOBALS['TL_LANG']['tl_fieldpalette']['modalTitle'] = '%s : %s';
