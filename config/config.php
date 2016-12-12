df87ec7a53ea0a65323e2b8f52dd8569d69e685c
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-12-12 15:52:16 +0100

    see cl

--- config/config.php
+++ config/config.php
@@ -38,12 +38,12 @@ $GLOBALS['TL_MODELS']['tl_fieldpalette'] = 'HeimrichHannot\FieldPalette\FieldPal
  */
 if (TL_MODE == 'BE')
 {
-    $GLOBALS['TL_JAVASCRIPT']['datatables-i18n'] = 'system/modules/fieldpalette/assets/vendor/datatables-i18n/datatables-i18n.min.js';
-    $GLOBALS['TL_JAVASCRIPT']['datatables-core'] = 'system/modules/fieldpalette/assets/vendor/datatables.net/js/jquery.dataTables.min.js';
-    $GLOBALS['TL_JAVASCRIPT']['datatables-rowReorder'] = 'system/modules/fieldpalette/assets/vendor/datatables-RowReorder/js/dataTables.rowReorder.min.js';
+    $GLOBALS['TL_JAVASCRIPT']['datatables-i18n'] = 'composer/vendor/heimrichhannot/datatables-additional/datatables-i18n/datatables-i18n.min.js';
+    $GLOBALS['TL_JAVASCRIPT']['datatables-core'] = 'composer/vendor/datatables/datatables/media/js/jquery.dataTables.min.js';
+    $GLOBALS['TL_JAVASCRIPT']['datatables-rowReorder'] = 'composer/vendor/heimrichhannot/datatables-additional/datatables-RowReorder/js/dataTables.rowReorder.min.js';
 
-    $GLOBALS['TL_CSS']['datatables-core'] = 'system/modules/fieldpalette/assets/vendor/datatables.net-dt/css/jquery.dataTables.min.css';
-    $GLOBALS['TL_CSS']['datatables-rowReorder'] = 'system/modules/fieldpalette/assets/vendor/datatables-RowReorder/css/rowReorder.dataTables.min.css';
+    $GLOBALS['TL_CSS']['datatables-core'] = 'composer/vendor/heimrichhannot/datatables-additional/datatables.net-dt/css/jquery.dataTables.min.css';
+    $GLOBALS['TL_CSS']['datatables-rowReorder'] = 'composer/vendor/heimrichhannot/datatables-additional/datatables-RowReorder/css/rowReorder.dataTables.min.css';
 
     $GLOBALS['TL_JAVASCRIPT']['fieldpalette-be.js'] = 'system/modules/fieldpalette/assets/js/fieldpalette-be.js' . (TL_MODE == 'BE' ? '' : '|static');
     $GLOBALS['TL_CSS']['fieldpalette-wizard-be']    = 'system/modules/fieldpalette/assets/css/fieldpalette-wizard-be.css';
